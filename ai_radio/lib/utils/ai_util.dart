73044506308ae563bf4baef03fa24705e70bf5c4
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2021-07-18 13:22:18 +0200

    Radio Player done
    Need to work on the AI part for voice

---
+++ ai_radio/lib/utils/ai_util.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color primaryColor1 = Vx.green800;
+  static Color primaryColor2 = Vx.yellow600;
+  static Color primaryColor3 = Vx.white;
+}
